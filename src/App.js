687b626dd34c59c1665decf69fdbabdca3062af6
GihongKim <creatvh@gmail.com>
2020-12-13 15:18:28 +0900

    Init
    Signed-off-by: GihongKim <creatvh@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import Movie from "./movie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const movies = [
+//   {id:0, title:"Matrix", poster : 'https://nerdist.com/wp-content/uploads/2019/08/matrix-keanue-reeves-759.jpg'},
+//   {id:1, title:"Full Metal Jacket", poster :"https://upload.wikimedia.org/wikipedia/en/3/39/The_Hunger_Games_cover.jpg"}
+// ]
+
+class App extends Component {
+  constructor() {
+    super();
+    console.log("Constructor");
+  }
+
+  // state = {
+  //   greeting: "Hello",
+  //   movies: [
+  //     {id:0, title:"Matrix", poster : 'https://nerdist.com/wp-content/uploads/2019/08/matrix-keanue-reeves-759.jpg'},
+  //     {id:1, title:"Full Metal Jacket", poster :"https://upload.wikimedia.org/wikipedia/en/3/39/The_Hunger_Games_cover.jpg"}
+  //   ]
+  // }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) => {
+      return (
+        <Movie
+          title={movie.title}
+          poster={movie.medium_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.summary}
+        />
+      );
+    });
+    return movies;
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({ movies });
+  };
+
+  _callApi = () => {
+    return fetch(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating&limit=5"
+    )
+      .then((res) => res.json())
+      .then((json) => json.data.movies)
+      .catch((err) => console.log(err));
+  };
+
+  componentDidMount() {
+    this._getMovies();
+  }
+
+  state = {};
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div className={movies ? "App" : "App-loading"}>
+        {this.state.movies ? this._renderMovies() : "Loading"}
+      </div>
+
+      // <div className="App">
+      //   {this.state.greeting}
+      // {this.state.movies.map((movie, index)=>{return <Movie title={movie.title} poster={movie.poster} key={index} />})}
+      // </div>
+    );
+  }
+
+  // componentDidMount() {
+  //   console.log('componentDidMount')
+  // }
 }
 
 export default App;
---
+++ src/movie.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./movie.css";
+
+// class Movie extends Component {
+
+//     static propTypes ={
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster} />
+//                 <h1> {this.props.title}</h1>
+//             </div>
+//             )
+//     }
+// }
+
+function Movie({ title, poster, genres, synopsis }) {
+  return (
+    <div className="Movie">
+      <div className="Movie_Columns">
+        <MoviePoster poster={poster} alt={title} />
+      </div>
+      <div className="Movie_Columns">
+        <h1> {title}</h1>
+        <div className="Movie_Genres">
+          {genres.map((genre, index) => (
+            <MovieGenre genre={genre} key={index} />
+          ))}
+        </div>
+        <p className="Movie_Synopsis">{synopsis}</p>
+      </div>
+    </div>
+  );
+}
+
+// class MoviePoster extends Component {
+
+//     static propTypes = {
+//         poster:PropTypes.string.isRequired
+//     }
+
+//     render() {
+//         return (<img src={this.props.poster} alt="movie Poster"></img>)
+//     }
+// }
+
+function MoviePoster({ poster, alt }) {
+  return (
+    <img src={poster} alt={alt} title={alt} className="Movie_Poster"></img>
+  );
+}
+
+function MovieGenre({ genre }) {
+  return <span className="Movie_Genres">{genre}</span>;
+}
+
+Movie.propTypes = {
+  poster: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired,
+};
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
+MovieGenre.prototype = {
+  genre: PropTypes.string.isRequired,
+};
+
+export default Movie;
